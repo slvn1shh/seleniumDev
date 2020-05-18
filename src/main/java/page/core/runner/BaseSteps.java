ecee46303ccc2e2aa25b5cbfe3b869d1b67bd3f8
slvn1shh <48070466+slvn1shh@users.noreply.github.com>
2020-05-18 14:24:36 +0300

    add start points to BaseSteps

--- src/main/java/page/core/runner/BaseSteps.java
+++ src/main/java/page/core/runner/BaseSteps.java
@@ -2,6 +2,8 @@ package page.core.runner;
 
 import io.qameta.htmlelements.WebPage;
 import io.qameta.htmlelements.WebPageFactory;
+import steps.AmazonCartSteps;
+import steps.AmazonItemSteps;
 import steps.AmazonUISteps;
 
 public class BaseSteps {
@@ -14,6 +16,13 @@ public class BaseSteps {
     public AmazonUISteps openMainPage() {
         driverManager.getDriver().navigate().to("https://amazon.com");
         return new AmazonUISteps(driverManager);
+    }
+    public AmazonItemSteps itemActions(){
+        return new AmazonItemSteps(driverManager);
+    }
+
+    public AmazonCartSteps proceedWithCart(){
+        return new AmazonCartSteps(driverManager);
     }
 
     protected <X extends WebPage> X on(Class<X> pageClass) {
